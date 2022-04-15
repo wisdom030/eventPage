a067ad272a0ee2d2d05f93a0422dfb715927fa92
wisdom030 <103177888+wisdom030@users.noreply.github.com>
2022-04-16 07:47:29 +0900

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+//메뉴
+jQuery(document).ready(function() {
+  $('.menu>li').mouseover(function() {
+    $(this).find('.submenu').stop().slideDown(500);
+  }).mouseout(function() {
+    $(this).find('.submenu').stop().slideUp(500);
+  });
+});
